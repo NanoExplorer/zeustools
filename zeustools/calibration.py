4b586ac0cd49f1ccc277d5d067feeb5859d6c681
Christopher Rooney <ctr_287@sbcglobal.net>
2022-07-05 13:26:09 -0400

    new blackbody code for power calculations

--- zeustools/calibration.py
+++ zeustools/calibration.py
@@ -35,7 +35,34 @@ def flat_to_wm2(sky_transparency,
     return calib_flux_density*bin_width
 
 
-def get_real_pwv(pwv,altitude):
+def bb_temp_watts(obs_wavelength,
+                  pixel_deltalambda,
+                  source_temp=270*units.K
+                  ):
+    """Get the power in W that the detectors see when
+    looking at a source. This is useful for dark IV tests and stuff
+
+    :param obs_wavelength: Astropy Quantity containing the wavelength observed
+    :param pixel_deltalambda: Astropy Quantity containing the difference in wavelength between light 
+        seen by the line pixel and a pixel next to it.
+        TODO: how linear is it really?
+    :param source_temp: astropy quantity source temperature - 77 K for LN2, ~285 K for roomtemp
+    """
+    frequency = (const.c/obs_wavelength).to(units.Hz)
+    bt = units.brightness_temperature(frequency)
+    brightness_temp = source_temp.to("Jy/steradian", equivalencies=bt)
+    spectral_stuff = units.spectral_density(obs_wavelength) 
+    zeus_beam_radius = np.arcsin(1/2.7/2)
+    zeus_beam_sr = zeus_beam_radius**2 * np.pi * units.steradian
+    zeus_400_px_size = 1.26e-3
+    zeus_400_px_area = zeus_400_px_size**2 * units.m**2
+    etendue = zeus_beam_sr * zeus_400_px_area
+    calib_flux_density = (brightness_temp*etendue).to("W / (m^2 Hz)", equivalencies=spectral_stuff)
+    bin_width = (const.c/obs_wavelength - const.c/(obs_wavelength+pixel_deltalambda)).to("Hz")
+    return calib_flux_density*bin_width
+
+
+def get_real_pwv(pwv, altitude):
     """Given the zenith PWV (reported by APEX) and altitude of source,
     returns the real amount of water between the telescope and space.
     
